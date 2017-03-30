b761a1760fe2b00d6778b2e64eca4999e031dc99
chuncheng <hi@chuncheng.me>
2017-03-30 23:14:56 +0800

    web is not supported

--- walle/web/tests/test_demo.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from walle.web.views.demo_view import WalleDemoView
-
-
-class TestDemo(unittest.TestCase):
-    def test_demo(self):
-        demo = WalleDemoView()
-        demo.get()
